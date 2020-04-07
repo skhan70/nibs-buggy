786173d66c32cdc94f965565ccecd9e474c3e40a
Durgesh Dhoot <ddhoot@salesforce.com>
2020-04-08 02:06:52 +0530

    Updated Comment

--- server/stores.js
+++ server/stores.js
@@ -7,6 +7,13 @@ var db = require('./pghelper'),
  * @param res
  * @param next
  */
+
+// ********************************************
+// A BUG HAS BEEN INTRODUCED on purpose to demonstrate Heroku flow
+// DELETE "WHERE name = 'bug' " in the SELECT QUERY below to fix the bug
+// ********************************************
+
+
 function findAll(req, res, next) {
     db.query("SELECT id, name, location__latitude__s AS latitude, location__longitude__s AS longitude FROM salesforce.store__c WHERE name = 'bug' ORDER BY name ASC")
         .then(function (stores) {
